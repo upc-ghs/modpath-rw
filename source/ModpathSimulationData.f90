776a65a8cf3771b3981ac17b407529a5cec299d9
Rodrigo Pérez <rodrigo.alfonso.perez@upc.edu>
2022-03-31 09:30:10 -0300

    minor cleaning in mpsimdata

--- source/ModpathSimulationData.f90
+++ source/ModpathSimulationData.f90
@@ -695,67 +695,16 @@ contains
                 this%TrackingOptions%observationUnits( nc ) = 5500 + nc
                 ! Write the cell ID
                 write( unit=tempChar, fmt=* )this%TrackingOptions%observationCells( nc )
+                print *,' OBSERVATION ', tempChar
                 ! Write the output file name
                 write( unit=this%TrackingOptions%observationFiles( nc ), fmt='(a)' )'cell_'//trim(adjustl(tempChar))//'.obs'
+                print *,' OBSERVATION FILENAME ', this%TrackingOptions%observationFiles( nc )
             end do 
 
         end if  
     end if 
 
 
-    !if ( ioInUnit .lt. 0 ) then
-    !    print *, 'ModpathSimulationData: END OF FILE'
-    !else
-    !    print *, 'ModpathSimulationData: WILL TRY OBSERVATION CELLS'
-
-    !    ! Now try to read if any observation cells
-    !    ! limit the number of observations to two, during dev
-    !    read(inUnit, * , iostat=ioInUnit) line
-    !    if ( ioInUnit .lt. 0 ) then 
-    !        ! No obs 
-    !        write(outUnit,'(A)') 'OBS: No observation cells.'
-    !        print *, 'ModpathSimulationData: NO OBSERVATION CELLS'
-    !    else 
-    !        print *, 'ModpathSimulationData: YES OBSERVATION CELLS', ioInUnit
-    !        ! Yes obs
-    !        icol = 1
-    !        call urword(line, icol, istart, istop, 2, n, r, 0, 0)
-    !        ! number of observations
-    !        if ( n .gt. 2 ) then 
-    !            ! out
-    !            call ustop('OBS: Max allowed observation cells equals 2. Stop.')
-    !        else if ( n .eq. 0 ) then 
-    !            ! no obs
-    !            write(outUnit,'(A)') 'OBS: No observation cells.'
-    !        else
-    !            ! ok
-    !            nObservations = n
-    !            write(outUnit,'(1X,A,I6,A)') 'OBS: ', nObservations, ' observation cells.'
-
-    !            ! Allocate observation arrays
-    !            call this%TrackingOptions%InitializeObservations( nObservations )
-
-    !            ! Read observation cells and assign 
-    !            ! proper variables
-    !            do nc = 1, nObservations
-    !                read(inUnit, '(a)', iostat=ioInUnit) line
-    !                icol = 1
-    !                call urword(line, icol, istart, istop, 2, n, r, 0, 0)
-    !                this%TrackingOptions%observationCells( nc ) = n
-    !                ! 5500 is just to move to a known place of id units, improve
-    !                this%TrackingOptions%observationUnits( nc ) = 5500 + nc
-    !                ! Write the cell ID
-    !                write( unit=tempChar, fmt=* )this%TrackingOptions%observationCells( nc )
-    !                ! Write the output file name
-    !                write( unit=this%TrackingOptions%observationFiles( nc ), fmt='(a)' )'cell_'//trim(adjustl(tempChar))//'.obs'
-    !            end do 
-
-    !        end if  
-    !    end if 
-
-    !end if  
-
-    !print *, 'ModpathSimulationData: LEAVING '
 
   end subroutine pr_ReadData
 
